6a64a76525d6496f54db9a68d4b00e6cc859c1e7
BrayanME0527 <bmonte.045@estudiantec.cr>
2023-09-04 12:22:10 -0600

    CódigoFInal

--- template_Proyecto_CE1.py
+++ template_Proyecto_CE1.py
@@ -9,123 +9,127 @@ Sistema de transmisión y recepción analógica
 @author: lcabrera
 """
 
-#importar bibliotecas utiles. De no tenerse alguna (import not found) se debe instalar, generalmente con pip
 import scipy.signal as signal
 from scipy.io import wavfile 
 import numpy as np
 import matplotlib.pyplot as plt
 import sounddevice as sd
 
-
-#definicion de 3 bloques principales: TX, canal y RX
+# Definición de 3 bloques principales: TX, canal y RX
 
 def transmisor(x_t):
+    # x_t debe ser una lista con múltiples arrays (caso de 3 señales) o una sola (caso del tono)
     
-    #x_t debe ser una lista con multiples array (caso de 3 señales) o una sola(caso del tono)
-    atono = "F:/TEC/tec/VIII Semestre/Comu/Proyecto/Etapa 1/datos_audio/tono.wav"
-    a = "F:/TEC/tec/VIII Semestre/Comu/Proyecto/Etapa 1/datos_audio/vowel_1.wav"
-    b = "F:/TEC/tec/VIII Semestre/Comu/Proyecto/Etapa 1/datos_audio/vowel_2.wav"
-    c = "F:/TEC/tec/VIII Semestre/Comu/Proyecto/Etapa 1/datos_audio/vowel_3.wav"
-    #Su codigo para el transmisor va aca
+    # Su código para el transmisor va aquí
     
-
-
+    s_t = x_t[0]  # eliminar cuando se tenga solución propuesta
     
-    ####################################
-    s_t=x_t[0] #eliminar cuando se tenga solucion propuesta
-    
-    return s_t #note que s_t es una unica señal utilizando un unico array, NO una lista
+    return s_t  # note que s_t es una única señal utilizando un único array, NO una lista
 
 def canal(s_t):
+    # Note que los parámetros mu (media) y sigma (desviación) del ruido blanco Gaussiano deben cambiarse según especificaciones
+    mu = 0
+    #sigma = 0.1
+    sigma = 500.1 #Sigma de prueba para poder graficar
     
-    #Note que los parámetros mu (media) y sigma (desviacion) del ruido blanco Gaussiano deben cambiarse segun especificaciones
-    mu=0;
-    sigma=0.1;
+    # Generar ruido gaussiano
+    noise = np.random.normal(mu, sigma, len(s_t))
     
-    #Su codigo para el canal va aca. 
-    noise = np.random.normal(mu, sigma, len(s_t))#ruido blanco
+    # Sumar el ruido a la señal original
     s_t_prima = s_t + noise
-
+    
     return s_t_prima
 
-
-def receptor(s_t_prima,f_rf):
+def receptor(s_t_prima, f_rf):
+    # Note que f_rf es la frecuencia utilizada para seleccionar la señal que se desea demodular
     
-    # Note que f_rf es la frecuencia utilizada para la seleccionar la señal que se desea demodular
+    # Su código para el receptor va aquí.
     
-    #Su codigo para el receptor va aca  
-       
-    
-    m_t_reconstruida=s_t_prima #eliminar cuando se tenga solucion propuesta
-    
-    #note que en el caso de multiples señales
+    m_t_reconstruida = s_t_prima  # eliminar cuando se tenga solución propuesta
     
     return m_t_reconstruida
 
-
-def plot_signal_vs_time(signal, sample_rate):
-    #print(len(signal))
-    time = np.arange(len(signal)) / sample_rate
+def plot_gaussian_noise(mu, sigma, num_samples):
+    noise = np.random.normal(mu, sigma, num_samples)
+    time = np.arange(num_samples)
+    
     plt.figure(figsize=(10, 6))
-    plt.plot(time, signal)
-    plt.xlabel('Tiempo (s)')
+    plt.plot(time, noise)
+    plt.xlabel('Tiempo (muestras)')
     plt.ylabel('Amplitud')
-    plt.title('Señal en función del tiempo')
+    #plt.title('Ruido Gaussiano en función del tiempo')
     plt.grid(True)
     plt.show()
 
+def plot_signal_vs_time(signal, sample_rate, duration):
+    num_samples = int(sample_rate * duration)
+    time = np.arange(num_samples) / sample_rate
+    plt.figure(figsize=(10, 6))
+    plt.plot(time, signal[:num_samples])
+    plt.xlabel('Tiempo (s)')
+    plt.ylabel('Amplitud')
+    #plt.title('Señal en función del tiempo')
+    plt.grid(True)
+    plt.show()
 
-################### Inicio de ejecucion #####################
-
-#Se da con ejemplo de tono, pasandolo por todo el sistema sin ningun cambio
-
-
-
+# Inicio de ejecución
+#TONO
 file_path = "C:/Users/bmont/OneDrive/Documentos/2023 Segundo Semestre/Comu 1/datos_audio/datos_audio/tono.wav"
+
+#AUDIO_1
+#file_path = "C:/Users/bmont/OneDrive/Documentos/2023 Segundo Semestre/Comu 1/datos_audio/datos_audio/vowel_2.wav"
 
 #leer tono desde archivo
 samplerate_tono, tono = wavfile.read(file_path)
 
+# Sobremuestreo para evitar problemas de aliasing (de necesitarse)
+resampling_factor = 4
+samples_new = len(tono) * resampling_factor
+samplerate_resampled = samplerate_tono * resampling_factor
+print('Cambiando frecuencia de muestreo de ' + str(samplerate_tono) + ' a ' + str(samplerate_resampled))
+tono_resampled = signal.resample(tono, samples_new).astype(np.int16)
 
-print("Sample Rate:", samplerate_tono)
-print("Audio Data Shape:", tono.shape)
+# Agregar el tono a la lista x_t requerida por el transmisor
+x_t = []  # solo para ejemplo, crear lista con el mismo tono 3 veces
+x_t.append(tono_resampled)
+x_t.append(tono_resampled)
+x_t.append(tono_resampled)
+#print("Se envía una lista con " + str(len(x_t)) + " señales")
 
+# Llamar función de transmisor
+s_t = transmisor(x_t)
 
-#oir tono rescatado. Esta funcion sirve tambien como transductor de salida 
-#Note la importancia de la frecuencia de muestreo (samplerate), la cual es diferente a la frecuencia fm del tono.
-
-#sd.play(tono, samplerate_tono)
-
-#Sobremuestreo para evitar problemas de aliasing (de necesitarse)
-#resampling_factor = 4
-#samples_new = len(tono) * resampling_factor
-#samplerate_resampled = samplerate * resampling_factor
-#print('Cambiando frecuencia de muestreo de '+str(samplerate)+' a '+str(samplerate_resampled))
-#data_resampled=signal.resample(tono, samples_new).astype(np.int16)
-#new_length=data_resampled.shape[0] / samplerate_resampled
-#time_resampled = np.linspace(0., new_length, data_resampled.shape[0])
-
-
-#agregar el tono a la lista X_t requerida por el transmisor
-x_t=[]  #solo para ejemplo, crear lista con el mismo tono 3 veces
-x_t.append(tono)
-x_t.append(tono)
-x_t.append(tono)
-print("Se envia una lista con "+str(len(x_t))+" señales")
-
-
-#llamar funcion de transmisor
-s_t=transmisor(x_t)
-
-# llamar funcion que modela el canal
+# Llamar función que modela el canal
 s_t_prima = canal(s_t)
 
-# llamar funcion de receptor
-m_t_reconstruida = receptor(s_t_prima, 1)#ojo que es f_rf de prueba
+# Sonido original
+print("Reproduciendo señal original:")
+sd.play(tono_resampled, samplerate=samplerate_resampled)
+sd.wait()
 
-# Sonido original con el ruido
-sd.play(m_t_reconstruida, samplerate_tono)
+# Sonido con ruido
+print("Reproduciendo señal con ruido:")
+sd.play(s_t_prima, samplerate=samplerate_resampled)
+sd.wait()
 
 
-# Graficar la señal en función del tiempo con ruido
-plot_signal_vs_time(m_t_reconstruida, samplerate_tono)
+# Llamar función de transmisor
+s_t = transmisor(x_t)
+
+# Llamar función que modela el canal
+s_t_prima = canal(s_t)
+
+# Llamar función de receptor
+m_t_reconstruida = receptor(s_t_prima, 1)  # ojo que es f_rf de prueba
+
+# Graficar el ruido gaussiano en función del tiempo (0 a 0.007 segundos)
+#Se debe de cambiar a 1 segundo si se quiere ver o esuchar normal
+duration = 0.007  # segundos
+
+plot_gaussian_noise(0, 0.1, len(s_t_prima))
+
+# Graficar la señal original en función del tiempo (0 a 0.007 segundos)
+plot_signal_vs_time(tono_resampled, samplerate_resampled, duration)
+
+# Graficar la señal con ruido en función del tiempo (0 a 0.007 segundos)
+plot_signal_vs_time(s_t_prima, samplerate_resampled, duration)
